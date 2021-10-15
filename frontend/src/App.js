b7f9e55c78930292c20ce565af29b98bb2ad26a3
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-10-15 11:39:27 +0530

    added libaries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GoogleLogin } from 'react-google-login'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const App = () => {
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
   return (
     <div>
       <h3>hello monster</h3>
+      <GoogleLogin
+        clientId="716248828673-kjpok8rlfaqv95m46vpjuk55hc6tpjso.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+
     </div>
   )
 }

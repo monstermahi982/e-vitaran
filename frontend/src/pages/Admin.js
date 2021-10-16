d17a6cf1833db7377bcc31f8a9dda87bed3cf330
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-10-16 19:33:46 +0530

    admin page updated

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+
 
 
 const Admin = () => {

273df075b51359fe2b60df54599cc46a2b93097b
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-10-16 19:34:11 +0530

    main page updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import AddEmployee from './pages/AddEmployee'
+import Admin from './pages/Admin'
+import Employee from './pages/Employee'
 // import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import NewConn from './pages/NewConn'
@@ -19,6 +22,15 @@ const App = () => {
           </Route>
           <Route path="/new-connection">
             <NewConn />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/employee">
+            <Employee />
+          </Route>
+          <Route path="/add-employee">
+            <AddEmployee />
           </Route>
           <Route path="/bill-calculator">
             <h4>THis is bill generator</h4>

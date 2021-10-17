f2531e92fbdf915f6ecd64e256abe5504736bfae
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-10-17 19:54:19 +0530

    bill unit bill values added, fetch-bill, pay-bill,add-bill fun added

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,58 @@ const userController = {
         res.json(documents);
     },
 
+
+    // pay bill handler
+    async payBill(req, res, next) {
+
+        let user;
+
+        try {
+            user = await User.findOneAndUpdate({ _id: req.params.id }, {
+                bill_unit: 0,
+                bill_status: "paid"
+            }, { new: true });
+
+
+        } catch (err) {
+            return next(err);
+        }
+
+        res.json({ user });
+    },
+
+    // bill update function
+    async billUpdate(req, res, next) {
+
+        const registerSchema = Joi.object({
+            bill_unit: Joi.number().required()
+        })
+
+        const { error } = await registerSchema.validate(req.body);
+
+        if (error) {
+            return next(error);
+        }
+
+        const { bill_unit } = req.body;
+
+        let user;
+
+        try {
+            user = await User.findOneAndUpdate({ _id: req.params.id }, {
+                bill_unit,
+                bill_status: "not paid"
+            }, { new: true });
+
+
+        } catch (err) {
+            return next(err);
+        }
+
+        res.json({ user });
+
+    },
+
     // fetching one user by id
     async getOneUsers(req, res, next) {
 
@@ -29,7 +81,23 @@ const userController = {
             return next(error);
         }
 
-        res.json({ documents });
+        res.json(documents);
+    },
+
+
+    async fetchBill(req, res, next) {
+        let documents;
+
+        try {
+            documents = await User.findOne({ phone: req.params.phone }).select('-updatedAt -__v');
+            if (!documents) {
+                return next(CustomErrorhandler.phoneNotFound('this number is not exists'));
+            }
+        } catch (error) {
+            return next(error);
+        }
+
+        res.json(documents);
     },
 
     // deleteing user

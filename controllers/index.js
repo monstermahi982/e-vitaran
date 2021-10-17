745ce3cf046d04a45c15c9f14a06fc0eed76c24a
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-10-17 19:56:03 +0530

    employee alias added

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,3 @@
 export { default as registerController } from './registerController'
+export { default as userController } from './userController'
+export { default as employeeController } from './employeeController'
